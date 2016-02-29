df2ca44f531d3fb2f0c2384f077a6185c3e305df
Ryan Ovrevik <rovrevik@ovrevik.com>
2016-02-29 12:00:40 -0500

    Add generated source from `yo angular site`

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name siteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the siteApp
+ */
+angular.module('siteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
